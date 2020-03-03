7ea3b948148e47feebc603814b99fb749ee6665d
Michel Pereira <michelbecauro@gmail.com>
2020-03-03 04:19:35 -0300

    Adicionadoo arquivo size_t.c
    Adicionei esse arquivo porque também foi de um assunto mencionado na aula.

---
+++ aula03/size_t.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+int main(void) {
+size_t i=0;
+
+// i++;
+
+// printf ("Tamanho de i (int) eh: %zu bytes / %zu bits\n", sizeof i, sizeof i *8);
+
+printf ("Valor de i eh: %zu\n", i);
+
+return 0;
+}
+
